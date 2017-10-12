0ac6ee8a5691ab00614791f95398ff7d4c853cc6
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2017-10-12 22:08:32 +0200

    Tried a bunch of stuff.

--- converter.py
+++ converter.py
@@ -1,3 +1,6 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i python3 -p python3 python36Packages.requests python36Packages.lxml
+
 import os
 import re
 import urllib
@@ -42,10 +45,7 @@ def findDiffMarkup(session, page, diff):
 
 	outTree = html.fromstring(out.text)
 	outList = outTree.xpath('//div[@id="wikitext"]')[0].getchildren()[5:-2]
-	acc = ""
-	for el in outList:
-		acc += etree.tostring(el,encoding='latin1', method='html', pretty_print=True).decode('utf8', 'replace')
-	return(acc)
+	return [etree.tostring(el, method='html', pretty_print=True) for el in outList]
 
 # Returns current verison of a page
 def findCurrentMarkup(session, page):
@@ -56,18 +56,17 @@ def findCurrentMarkup(session, page):
 # Returns a touple with the pagename and a dictinoray containing all versions of
 # the page as (diff, author, comment)
 def pageVersions(file):
-    f = open(file, 'r')
-    fileText = f.read()
-    name = re.search('(?:name=)(.*)', fileText)
-    if name is None:
-        print("No name", file)
-        return {}
-    else:
-        name = name.group(1)
-        regex = re.compile('author:(?P<diff>\d{10})=(?P<author>[\w\/]*)\s(?:csum:\d{10}=(?P<comment>.*))?', re.MULTILINE)
-        m = re.findall(regex, fileText)
-        f.close
-    return name, m
+        with open(file, 'r') as f:
+                fileText = f.read().decode("latin1")
+                name = re.search('(?:name=)(.*)', fileText)
+                if name is None:
+                        print("No name", file)
+                        return {}
+                else:
+                        name = name.group(1)
+                        regex = re.compile('author:(?P<diff>\d{10})=(?P<author>[\w\/]*)\s(?:csum:\d{10}=(?P<comment>.*))?', re.MULTILINE)
+                        m = re.findall(regex, fileText)
+                        return name, m
 
 # Returns a dictionary containing all different versions of a page as
 # (diff, author, comment, content)
@@ -83,7 +82,7 @@ def pageConverter(session, file):
             n+=1
     elif len(diffs) == 1: # If only one version of the page exists get current source instead
         content = findCurrentMarkup(session, name)
-        diff_number, author, comment = diffs[0]
+        diff_number, author, comment = diffs[n0]
         diffs2[0] = {'diff':int(diff_number), 'author':author, 'comment':comment, 'content':content}
     else:
         print("Empty", file)
@@ -104,20 +103,24 @@ def findAndConvertPages(session, directory, types, ignore_names):
 		# Convert page if it is not on the ignore list or a deleted page
 		if (pagetype in types) and (pagename not in ignore_names) and (len(page.split(',')) < 2):
 			if count >= offset and not os.path.isfile('converted/'+page):
-				f = open('converted/'+page, 'w')
-				pagecontent = pageConverter(session, directory+page)
-				f.write(json.dumps(pagecontent, sort_keys=True, indent=4, separators=(',', ': ')))
-				f.close
-			elif os.path.isfile('converted/'+page):
-				print(page+" is already parsed, if it is not finished, remove this file.")
+				with open('converted/'+page, 'w') as f:
+				        pagecontent = pageConverter(session, directory+page)
+				        f.write(json.dumps(pagecontent, sort_keys=True, indent=4, separators=(',', ': ')))
+			elif os.path.isfile('converted/' + page):
+				print(page +" is already parsed, if it is not finished, remove this file.")
 		count += 1
 
 def main(filename, username, password):
-	s = requests.Session()
+        s = requests.Session()
 	s.post(baseUrl + "Main/LoginPage?action=login", data={"username" : username,"password" : password})
 #	pageConverter(s, "/home/swij/Kod/wiki/wiki.d/Profiles.Rövgoat")
-	findAndConvertPages(s, "/home/swij/Kod/wiki/wiki.d/", ["Main", "Profiles"], ["RecentChanges", "GroupAttributes", "Profiles", "Rövgoat"])
+	findAndConvertPages(s, "/home/jassob/Projects/pm-wiki-exporter/wiki.d/", ["Main", "Profiles"], ["RecentChanges", "GroupAttributes", "Profiles", "Rövgoat"])
 	#print(findAndConvertPages(s, "/home/swij/Kod/wiki/converted/", ["Main","Profiles"]))
 	#print (json.dumps(pageConverter(s, filename)))
 
-main("", "username", "password")
+main("", "simonwi", "hackehackspett")
+
+
+# Local Variables:
+# tab-width: 4
+# End:
