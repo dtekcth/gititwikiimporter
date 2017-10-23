080da9cb86c44db0269f7ae8203921b277538cca
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2017-10-24 00:19:00 +0200

    Rewrote almost everything.
    Type hints!

--- converter.py
+++ converter.py
@@ -6,124 +6,145 @@
 
 import os
 import re
-import urllib
+import urllib.parse
 import requests
 import json
 import time
+import lxml.html as LH
 
-from lxml import html, etree
+from collections import namedtuple
+from lxml import etree
 
-baseUrl = "https://demeter.dtek.se/wiki/"
+from typing import *
 
-# Returns the pmwiki markup of a specific version of a page
-def findDiffMarkup(session, page, diff):
-	print(page, diff)
-	pageWithSlash = page.replace(".", "/")
-	response = session.get(baseUrl + pageWithSlash + "?action=edit&restore=diff:"+diff+":"+diff+"&preview=y")
+baseUrl: str = "https://demeter.dtek.se/wiki/"
 
-	tree = html.fromstring(response.content)
-	basetime = tree.xpath('//input[@name="basetime"]')
+class Revision(NamedTuple):
+    diff: int
+    author: str
+    comment: str
+    content: Optional[str]
 
-	if len(basetime) == 0:
-		print("Invalid", page, diff)
-		return ""
+    def has_content(self) -> bool:
+        return self.content is not None
 
-	content = tree.xpath('//textarea[@id="text"]/text()')
-	if len(content) == 0:
-		return ""
 
-	data = {
-		'action':'edit',
-		'n':page,
-		'basetime':basetime[0].value,
-        	'text':content[0],
-        	'csum':'',
-        	'author':'simonwi',
-        	'preview':'+Förhandsgranska+'
-	}
+def set_content(r: Revision, content: str) -> Revision:
+    """Returns a new Revision with updated content"""
+    return Revision(r.diff, r.author, r.comment, content)
 
-	encoded = urllib.parse.urlencode(data, encoding='ISO-8859-1')
+# Type aliases
+Session = requests.sessions.Session
+Response = requests.Response
+FilePath = str
 
-	out = session.post(baseUrl + page + "?action=edit", data, session.headers)
+def findDiffMarkup(session: Session, page: FilePath, diff: str) -> Optional[str]:
+    """
+    Extracts the HTML of the revision by requesting the PmWiki instance
+    to render a preview of said revision
+    """
+    print(page, diff)
+    pageWithSlash = page.replace(".", "/")
+    response = session.get(baseUrl + pageWithSlash + "?action=edit&restore=diff:"+diff+":"+diff+"&preview=y")
 
-	outTree = html.fromstring(out.text)
-	outList = outTree.xpath('//div[@id="wikitext"]')[0].getchildren()[5:-2]
-	return [etree.tostring(el, method='html', pretty_print=True) for el in outList]
+    tree = LH.fromstring(response.content)
+    basetime = tree.xpath('//input[@name="basetime"]')
+    content = tree.xpath('//textarea[@id="text"]/text()')
 
-# Returns current verison of a page
-def findCurrentMarkup(session, page):
-    page = page.replace(".", "/")
-    response = session.get(baseUrl + page + "?action=source")
-    return response.text
+    if len(basetime) == 0:
+        print("Invalid", page, diff)
+        return None
 
-# Returns a touple with the pagename and a dictinoray containing all versions of
-# the page as (diff, author, comment)
-def pageVersions(file):
-        with open(file, 'r') as f:
-                fileText = f.read().decode("latin1")
-                name = re.search('(?:name=)(.*)', fileText)
-                if name is None:
-                        print("No name", file)
-                        return {}
-                else:
-                        name = name.group(1)
-                        regex = re.compile('author:(?P<diff>\d{10})=(?P<author>[\w\/]*)\s(?:csum:\d{10}=(?P<comment>.*))?', re.MULTILINE)
-                        m = re.findall(regex, fileText)
-                        return name, m
+    if len(content) == 0:
+        return None
 
-# Returns a dictionary containing all different versions of a page as
-# (diff, author, comment, content)
-def pageConverter(session, file):
-    name, diffs = pageVersions(file)
-    diffs2 = {}
+    data = {
+        'action':'edit',
+        'n':page,
+        'basetime':basetime[0].value,
+        'text':content[0],
+        'csum':'',
+        'author':'simonwi',
+        'preview':'+Förhandsgranska+'
+    }
 
-    if len(diffs) > 1:
-        n = 0
-        for diff_number, author, comment in diffs:
-            content = findDiffMarkup(session, name, diff_number)
-            diffs2[n] = {'diff':int(diff_number), 'author':author, 'comment':comment, 'content':content}
-            n+=1
-    elif len(diffs) == 1: # If only one version of the page exists get current source instead
-        content = findCurrentMarkup(session, name)
-        diff_number, author, comment = diffs[n0]
-        diffs2[0] = {'diff':int(diff_number), 'author':author, 'comment':comment, 'content':content}
-    else:
-        print("Empty", file)
-    return diffs2
+    response = session.post(baseUrl + page + "?action=edit", data, session.headers)
+    return extractHtml(response)
 
-# Finds all pages in a directory and turns them into a dictionary
-def findAndConvertPages(session, directory, types, ignore_names):
-	if not os.path.exists("converted"):
-	    os.mkdir("converted")
-	pages = os.listdir(directory)
+def extractHtml(response: Response) -> str:
+    """Extracts the html inside the 'wikitext'-div"""
+    elements = LH.fromstring(response.content).xpath('//div[@id="wikitext"]')[0].getchildren()
+    return "\n".join(str(etree.tostring(el, encoding='unicode', method='html', pretty_print=True)) for el in elements)
 
-	# Offset for starting converter at specific point, 0 = default
-	# 563 = dHack (header crash)
-	offset = 0
-	count = 1
-	for page in pages:
-		pagetype, pagename = page.split('.',2)
-		# Convert page if it is not on the ignore list or a deleted page
-		if (pagetype in types) and (pagename not in ignore_names) and (len(page.split(',')) < 2):
-			if count >= offset and not os.path.isfile('converted/'+page):
-				with open('converted/'+page, 'w') as f:
-				        pagecontent = pageConverter(session, directory+page)
-				        f.write(json.dumps(pagecontent, sort_keys=True, indent=4, separators=(',', ': ')))
-			elif os.path.isfile('converted/' + page):
-				print(page +" is already parsed, if it is not finished, remove this file.")
-		count += 1
+def findRevisionsMeta(file: FilePath) -> Tuple[str, List[Revision]]:
+    """Finds the metadata about revisions for a page"""
+    with open(file, 'r') as f:
+        fileText = f.read()
+        match = re.search('(?:name=)(.*)', fileText)
 
-def main(filename, username, password):
-        s = requests.Session()
-	s.post(baseUrl + "Main/LoginPage?action=login", data={"username" : username,"password" : password})
-#	pageConverter(s, "/home/swij/Kod/wiki/wiki.d/Profiles.Rövgoat")
-	findAndConvertPages(s, "/home/jassob/Projects/pm-wiki-exporter/wiki.d/", ["Main", "Profiles"], ["RecentChanges", "GroupAttributes", "Profiles", "Rövgoat"])
-	#print(findAndConvertPages(s, "/home/swij/Kod/wiki/converted/", ["Main","Profiles"]))
-	#print (json.dumps(pageConverter(s, filename)))
+        name = match.group(1)
+        regex = re.compile('author:(?P<diff>\d{10})=(?P<author>[\w\/]*)\s(?:csum:\d{10}=(?P<comment>.*))?', re.MULTILINE)
+        revisions = []
+
+        for matches in re.finditer(regex, fileText):
+            revisions.append(Revision(int(matches.group('diff')), matches.group('author'),
+                                      matches.group('comment'), None))
+
+        return name, revisions
+
+def findRevisions(session: Session, file: FilePath) -> List[Revision]:
+    """Return the revisions for a page"""
+    name, revisions = findRevisionsMeta(file)
+
+    return list(set_content(r, findDiffMarkup(session, name, str(r.diff))) for r in revisions)
+
+def findAndConvertPages(session: Session, directory: FilePath, namespaces: List[str],
+                        ignored_names: List[str], offset = 0) -> None:
+    """
+    Creates converted copies of PmWiki page-files in converted/ directory.
+
+    The converted files will be in a serialized JSON format:
+    { 'diff': int, 'author': str, 'comment': str, 'content': html }
+
+
+    Where 'diff' is some kind of timestamp, 'author' is a user account on
+    the PmWiki instance, 'comment' is just an ordinary string and 'content'
+    is a python string representation of the html of that revision.
+
+    Only pages within the specified namespaces will be converted and that are not
+    in the skipped_names-list (without namespace).
+    """
+
+    # Create directory structure
+    if not os.path.exists("converted"):
+        os.mkdir("converted")
+        for ns in namespaces:
+            if not os.path.exists(f"converted/{ns}"):
+                os.mkdir(f"converted/{ns}")
+
+    def page_predicate(page: str) -> bool:
+        """Simple predicate for removing unwanted pages"""
+        parts = page.split(".", 1)
+        return ("," not in page) and (parts[0] in namespaces) and (parts[1] not in ignored_names)
+
+
+    pages = (page.split(".", 1) for page in sorted(os.listdir(directory)[offset:]) if page_predicate(page))
+
+    for (page_ns, page_name) in pages:
+        if not os.path.isfile(f"converted/{page_ns}/{page_name}"):
+            with open(f'converted/{page_ns}/{page_name}', 'w') as f:
+                f.write(json.dumps(findRevisions(session, directory+page_ns+"."+page_name),
+                                   sort_keys=True, indent=4, separators=(',', ': ')))
+
+        # if the file already exists we assume it has been converted already
+        else:
+            print(f"converted/{page_ns}/{page_name} is already parsed, if it is not finished, remove this file.")
+
+def main(filename: str, username: str, password: str) -> None:
+    with requests.Session() as s:
+        s.post(baseUrl + "Main/LoginPage?action=login" ,
+               data={"username": username,"password": password})
+        findAndConvertPages(s, "/home/jassob/Projects/pm-wiki-exporter/wiki.d/",
+                            ["Main", "Profiles"], ["RecentChanges", "GroupAttributes", "Profiles"])
 
 main("", "simonwi", "hackehackspett")
-
-
-# Local Variables:
-# tab-width: 4
-# End:
